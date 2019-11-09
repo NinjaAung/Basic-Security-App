bdc3b4946256d2aedd78e8ad76cfad4691225338
ninjaaung <ninja.aung02@gmail.com>
2019-11-09 11:44:14 -0800

    New Changes

--- app.py
+++ app.py
@@ -22,7 +22,9 @@ def index():
     return render_template('index.html')
 
 
-
+@app.route('/2')
+def index2():
+    return render_template('index2.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 5000)) 
